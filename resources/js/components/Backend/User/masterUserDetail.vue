d86949ce3f22b6e9a02e3a54e51f39e897650ee7
Samyam Kafle <samyam1kafle@gmail.com>
2020-05-26 23:57:25 +0545

    Profile image and name in master blade converted into vue components

---
+++ resources/js/components/Backend/User/masterUserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            <img :src="getUserImage(user.display_image)"
+                    class="img-circle elevation-2"
+                    alt="User Image">
+        </div>
+        <div class="info">
+            <a href="#" class="d-block">{{user.name}}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {userEvent} from '../../../app.js'
+
+    export default {
+        props: ['auth_user'],
+        data() {
+            return {
+                user: this.auth_user
+            }
+        },
+        methods:{
+            getUserImage(image){
+                let photo = 'Backend/ProfileImages/' + image;
+                return photo;
+            }
+        },
+
+        created(){
+            userEvent.$on('userUpdated',(data)=>{
+                this.user = data
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -66,16 +66,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Sidebar -->
         <div class="sidebar">
             <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="{{asset('Backend/ProfileImages/'. auth()->user()->display_image)}}"
-                         class="img-circle elevation-2"
-                         alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block">{{auth()->user()->name}}</a>
-                </div>
-            </div>
+            <user-details :auth_user="{{auth()->user()}}"></user-details>
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
